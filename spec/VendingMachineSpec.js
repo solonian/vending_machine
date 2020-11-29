11d93e26bf5dba308fb84fe0d84ee30ffdaa135c
Paul Nolan <pnolan@solonian.co.uk>
2020-11-29 15:41:44 +0000

    test for dispensing the product

--- spec/VendingMachineSpec.js
+++ spec/VendingMachineSpec.js
@@ -65,6 +65,38 @@ describe("Vending Machine", function() {
       expect(vendingMachine.selectedProduct.price).toEqual(1.0);
     });
 
+    it("should show the price to pay when a product is selected", function(){
+      vendingMachine.productSelected("COLA");
+      expect(vendingMachine.display).toEqual("Cola : $1.00");
+    });
+
+
+    // this is a departure from the desription in the Select Product section - as that seem to contradict the preceding section
+    // In the absence of a defining Product Owner - retain the higher level of functionality
+    it("should recalculate the remaining balance when coins are introduced", function(){
+      vendingMachine.productSelected("COLA");
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      expect(vendingMachine.display).toEqual("Cola : $0.75");
+    });
+
+    it("should show the correct balance when coins are introduced before selecting the product", function(){
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      vendingMachine.productSelected("COLA");
+      expect(vendingMachine.display).toEqual("Cola : $0.75");
+    });
+
+    it("should dispense the product when the amount matches or exceeds the price", function(){
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      vendingMachine.acceptCoin(5.67); // weight of quarter = 0.25
+      vendingMachine.productSelected("COLA");
+      // expect(vendingMachine.display).toEqual("Thank You");
+      expect(vendingMachine.selectedProduct).toBe(null);
+      expect(vendingMachine.amountIntroduced).toEqual(0);
+
+    });
+
   });
 
 
